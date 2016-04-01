76c4f66b0616b002f4ebcd808a449536bef390c0
Anthony Tellez <anthonygtellez@gmail.com>
2016-03-31 19:54:24 -0500

    Update remove_fishbucket.bat

--- fw_delete_fishbucket_windows/bin/remove_fishbucket.bat
+++ fw_delete_fishbucket_windows/bin/remove_fishbucket.bat
@@ -1,7 +1,7 @@
 @echo off
-if exist "C:\Program Files\SplunkUniversalForwarder\.fishbucket" exit
+if exist "C:\Program Files\SplunkUniversalForwarder\fishbucket_cleared" exit
 timeout /t 5 /nobreak
-md "C:\Program Files\SplunkUniversalForwarder\.fishbucket"
+md "C:\Program Files\SplunkUniversalForwarder\fishbucket_cleared"
 NET STOP SplunkForwarder
 @RD /S /Q "C:\Program Files\SplunkUniversalForwarder\var\lib\splunk\fishbucket"
 timeout /t 30 /nobreak
